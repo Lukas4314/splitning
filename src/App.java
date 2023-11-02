ab0d33b3651a8ec3c7542738e95fde6af76354e2
Lukas4314 <lukashjeronimus@icloud.com>
2023-11-02 09:43:48 +0100

    de bevæger sig

--- src/App.java
+++ src/App.java
@@ -8,25 +8,43 @@ public class App extends PApplet {
     ArrayList<PVector> particlesPos = new ArrayList<PVector>();
     ArrayList<PVector> particlesVel = new ArrayList<PVector>();
 
+    int size = 50;
+    int startValue = 200;
+
     public static void main(String[] args) {
         PApplet.main("App");
     }
 
     public void settings() {
-        size(500, 500);
+        size(1600, 1000);
     }
 
     public void setup() {
-        for (int i = 0; i < 1; i++) {
+        for (int i = 0; i < startValue; i++) {
             particlesPos.add(new PVector(random(width), random(height)));
+            particlesVel.add(new PVector(random(10) - 5, random(10) - 5));
         }
     }
 
     public void draw() {
         background(0);
         fill(255, 0, 0);
+
+        for (int i = 0; i < particlesVel.size(); i++) {
+            if (particlesPos.get(i).x >= width || particlesPos.get(i).x <= 0) {
+                particlesVel.get(i).x = -particlesVel.get(i).x;
+            }
+            if (particlesPos.get(i).y >= height || particlesPos.get(i).y <= 0) {
+                particlesVel.get(i).y = -particlesVel.get(i).y;
+            }
+        }
+
+        for (int i = 0; i < particlesVel.size(); i++) {
+            particlesPos.get(i).x = particlesPos.get(i).x + particlesVel.get(i).x;
+            particlesPos.get(i).y = particlesPos.get(i).y + particlesVel.get(i).y;
+        }
         for (PVector dot : particlesPos) {
-            circle(dot.x, dot.y, 10);
+            circle(dot.x, dot.y, size);
         }
     }
 }
