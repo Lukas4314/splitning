65155ce39601e39e693777b6ddd60e03dedc991d
Lukas4314 <lukashjeronimus@icloud.com>
2023-11-02 09:30:01 +0100

    lavede en cirkel

--- src/App.java
+++ src/App.java
@@ -1,7 +1,13 @@
+import java.util.ArrayList;
+
 import processing.core.PApplet;
+import processing.core.PVector;
 
 public class App extends PApplet {
-   
+
+    ArrayList<PVector> particlesPos = new ArrayList<PVector>();
+    ArrayList<PVector> particlesVel = new ArrayList<PVector>();
+
     public static void main(String[] args) {
         PApplet.main("App");
     }
@@ -11,9 +17,16 @@ public class App extends PApplet {
     }
 
     public void setup() {
-   
+        for (int i = 0; i < 1; i++) {
+            particlesPos.add(new PVector(random(width), random(height)));
+        }
     }
 
     public void draw() {
-     
+        background(0);
+        fill(255, 0, 0);
+        for (PVector dot : particlesPos) {
+            circle(dot.x, dot.y, 10);
+        }
     }
+}
