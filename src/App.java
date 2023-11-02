fbfd6a5fca22d378fd18bc7790bbe20992c5de42
Lukas4314 <lukashjeronimus@icloud.com>
2023-11-02 09:17:56 +0100

    lavede et tomt doc

---
+++ src/App.java
@@ -0,0 +1,19 @@
+import processing.core.PApplet;
+
+public class App extends PApplet {
+   
+    public static void main(String[] args) {
+        PApplet.main("App");
+    }
+
+    public void settings() {
+        size(500, 500);
+    }
+
+    public void setup() {
+   
+    }
+
+    public void draw() {
+     
+    }
